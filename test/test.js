3cf92d4e440ef159d21a90f622dd5a7d70d83f0b
Cameron Gordon <cameron.gordon@ledgerfi.co>
2024-04-01 11:30:35 +0200

    Add test.js

---
+++ test/test.js
@@ -0,0 +1,14 @@
+let assert = require("assert");
+let Phrase = require("../index.js");
+
+describe ("Phrase", function() {
+
+    describe ("#palindrome", function() {
+
+        it("should return false for a non-palindrome", function() {
+            let nonPalindrome = new Phrase("Apple");
+            assert(!nonPalindrome.palindrome());
+        });
+    });
+
+});
