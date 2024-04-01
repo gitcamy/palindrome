8e629caea546634a7b141e70506cb61ba9f5ade1
Cameron Gordon <cameron.gordon@ledgerfi.co>
2024-04-01 11:29:17 +0200

    fixed palindrome test

--- index.js
+++ index.js
@@ -1,41 +1,20 @@
 module.exports = Phrase;
 
 //Add `reverse` to all strings 
-String.prototype.reverse = function() {
+String.prototype.reverse = function reverse() {
     return Array.from(this).reverse().join("");
-}
-
-//add blank method that returns true if string is blank
-String.prototype.blank = function() {
-    return !!(this.match(/^\s*$/g));
-}
-
-//return last element of an Array
-Array.prototype.last = function () {
-    return this[this.length - 1];
-}
-
-//Returns true for a palindrome
-function palindrome(string) {
-    let processedContent = string.toLowerCase();
-    return processedContent === processedContent.reverse();
 }
 
 //defines a Phrase objec†
 function Phrase(content) {
     this.content = content;
 
-    //returns lowercased content
-    this.processor = function(string) {
-        return string.toLowerCase();
-    }
-
-    //Returns processed content for palindrome
+    //Returns processed content for palindrome testing
     this.processedContent = function processedContent() {
-        return this.processor(this.content);
+        return this.content.toLowerCase();
     };
 
-    //Returns true for a palindrome
+    //Returns true for a palindrome, false otherwise
     this.palindrome = function palindrome() {                     
         return this.processedContent() === reverse(this.processedContent());
     };
